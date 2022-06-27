f62a20744ccffcc1916929d580090d5de0eaeb38
Jake McHargue <jake.mchargue@gmail.com>
2022-06-27 10:39:44 -0600

    get a bunch of ids

--- app.rb
+++ app.rb
@@ -4,12 +4,13 @@ require 'json'
 HOST = 'nodes-on-nodes-challenge.herokuapp.com'
 ROUTE = '/nodes/'
 INITIAL_PARENT_NODE_ID = '089ef556-dfff-4ff2-9733-654645be56fe'
-CHILD_NODE_KEY = "child_node_ids"
+CHILD_NODES_KEY = "child_node_ids"
 
 def append_network_nodes(id_list, parent_id)
   next_nodes = JSON.parse(Net::HTTP.get(HOST, ROUTE + parent_id))
   next_nodes.each do |node|
-  	id_list.push node[CHILD_NODE_KEY]
+  	id_list.push node["id"]
+  	node[CHILD_NODES_KEY].each { |id| append_network_nodes(id_list, id) }
   end
 end
 
